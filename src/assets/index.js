894ed158a88bbbb7c2ca6eca8f3fdb76c9f807d6
Bowman1410 <iainbow1410@gmail.com>
2024-01-15 07:54:40 +0200

    First commit

---
+++ src/assets/index.js
@@ -0,0 +1,86 @@
+import logo from "./logo.svg";
+import logo11 from "./logo11.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import python from "./tech/python.png";
+import pandas from "./tech/pandas.png";
+import numpyicon from "./tech/numpyicon.png";
+import matplotlib from "./tech/matplotlib.png";
+import sklearn from "./tech/sklearn.png";
+import tensorflow from "./tech/tensorflow.png";
+import spark from "./tech/spark.png";
+import sql from "./tech/sql.png";
+import jupyter from "./tech/jupyter.jpg";
+import aws from "./tech/aws.jpg";
+import docker from "./tech/docker.png";
+import githublogo from "./tech/githublogo.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  pandas,
+  numpyicon,
+  matplotlib,
+  sklearn,
+  tensorflow,
+  spark,
+  sql,
+  jupyter,
+  aws,
+  githublogo,
+  logo,
+  logo11,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  python,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
