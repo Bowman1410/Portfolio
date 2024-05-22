57bc2c9c03c4f87f622cd7efb7e1978c9f17f916
Bowman1410 <iainbow1410@gmail.com>
2024-05-22 11:31:45 +0200

    6

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Portfolio/", // Update <REPO_NAME> to your actual repository name
+});
